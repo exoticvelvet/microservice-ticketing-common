2ff005ecf21977be0a38a4bebf995fd8045b5181
exoticvelvet <1198275509@qq.com>
2021-04-05 14:34:21 +0100

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 10,
+  green: 10,
+};
+
+console.info(color);
+
+export default color;
